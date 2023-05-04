cdc78b06e7f8ad212f96849b8186e3f12f3c318f
Stanislav <people.ss@yandex.ru>
2023-05-04 16:54:52 +0300

    Addition commit

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -125,4 +125,52 @@ public class TodosTest {
         Task[] actual = todos.search("Приложение НетоБанка");
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldSearchTwoTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Приложение НетоБанка");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, meeting};
+        Task[] actual = todos.search("Приложение НетоБанка");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNothing() {
+        SimpleTask simpleTask = new SimpleTask(5, "Приложение НетоБанка");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { };
+        Task[] actual = todos.search("Приложение Нетологии");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
